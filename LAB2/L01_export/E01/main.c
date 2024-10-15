96f5a3b618bb02bba73cf486857538067b99aaac
diegocampanale <campanale.die@gmail.com>
2024-10-15 21:43:21 +0200

    resetup

---
+++ LAB2/L01_export/E01/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define INPUTFILE_PATH "./in.txt"
+
+int gcd(int a, int b);
+
+int main(int argc, char ** argv){
+
+    printf("gcd: %d",gcd(600,54));
+}
+
+int gcd(int a, int b){
+
+    if (a<b){
+        int temp = b;
+        b = a;
+        a = temp;
+    }
+
+    if (b==0) return a;
+    if (b==1) return 1;
+
+    if ((a%2 == 0) && (b%2==0)){ // a,b even
+        return 2*gcd(a/2, b/2);
+    }
+    if (a%2==0 && b%2) { // a even, b odd
+        return gcd(a/2, b);
+    }
+    if (a%2 && b%2==0) { // a odd, b even
+        return gcd(a, b/2);
+    }
+    if (a%2 && b%2) { // a,b odd
+        return gcd((a-b)/2, b);
+    }
+}
---
+++ LAB2/cmake-build-debug/E03/CMakeFiles/E03.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/E03.dir/__/E01/main.c.o"
+  "CMakeFiles/E03.dir/__/E01/main.c.o.d"
+  "CMakeFiles/E03.dir/main.c.o"
+  "CMakeFiles/E03.dir/main.c.o.d"
+  "E03"
+  "E03.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/E03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
