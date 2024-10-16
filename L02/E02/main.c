31ddfa1d21f1f545b1fcfd9ade5c99bb38109524
Campanale  Diego <s325040@studenti.polito.it>
2024-10-16 12:47:15 +0200

    Esercizio 2 Lab 2

--- L02/E02/main.c
+++ L02/E02/main.c
@@ -1,17 +1,107 @@
 #include <stdio.h>
+#include <malloc.h>
+#include <string.h>
 
-#define INPUTFILE_PATH "/Users/diegocampanale/CLionProjects/ASD_24/L02/E02/mat.txt"
+#define INPUTFILE_PATH "C:\\Users\\s325040\\CLionProjects\\ADS_2024\\L02\\E02\\mat.txt"
 
-void separa(int **mat, int nr, int nc );
+void separa(int **mat, int nr, int nc, int** b, int** w);
+int **malloc2dR(int nr, int nc);
+void malloc2dP(int ***mp, int nr, int nc);
+void stampaMat(int **m, int nr, int nc);
 
 int main(int argc, char ** argv){
-    int nr, nc, i;
+    int nr, nc, **matR, **matP, *black, *white;
+
     FILE *fp = fopen(INPUTFILE_PATH, "r");
     fscanf(fp, "%d %d ", &nr, &nc);
-    printf("%d %d ", nr, nc);
-    for (i = 0; i<nr; i++){
+    //printf("%d %d \n", nr, nc);
 
+    matR = malloc2dR(nr,nc);
+    malloc2dP(&matP,nr,nc);
+
+    // leggi matrice
+    int i,j, val;
+    for(i=0;i<nr;i++) {
+        for (j = 0; j < nc; j++) {
+            fscanf(fp, "%d ", &val);
+            matR[i][j] = val;
+            matP[i][j] = val;
+            //printf("%d %d\n", val, matP[i][j]);
+        }
+        fscanf(fp, "\n");
+        //printf("\n");
     }
 
+    //stampaMat(matR, nr, nc);
+    //stampaMat(matP,nr,nc);
+    separa(matP,nr,nc, &black, &white);
+
+    //stampa vettori
+    int len_black = (nr*nc)/2;
+    int len_white = (nr*nc)-len_black;
+
+    printf("Vettore bianchi:\n");
+    for(j=0; j<len_white;j++){
+        printf("%d ", white[j]);
+    }
+    printf("\nVettore neri:\n");
+    for(j=0; j<len_black;j++){
+        printf("%d ", black[j]);
+    }
+    free(black);
+    free(white);
+
+    fclose(fp);
     return 0;
+}
+
+void separa(int **mat, int nr, int nc, int** b, int** w){
+    int i,j, nb = (nr*nc)/2, nw = (nr*nc)-nb, ib,iw;
+    *b = malloc(nb*sizeof(int));
+    *w = malloc(nw*sizeof(int));
+
+    ib=0;iw=0;
+    // scorre la matrice finchè i vettori b (black) e w(white) sono completamente pieni
+        for (i=0;i<nr;i++){
+            for(j=0; j<nc;j++){
+                if((i+j)%2==0){       // i+j pari -> bianco
+                    (*w)[iw] = mat[i][j];
+                    iw++;
+                }else{              // i+j dispari -> nero
+                    (*b)[ib] = mat[i][j];
+                    ib++;
+                }
+                //printf("%d %d \n", iw, ib);
+            }
+        }
+
+}
+
+void stampaMat(int **m, int nr, int nc){
+    int i,j;
+    // Stampa
+    for(i=0;i<nr;i++){
+        for(j=0;j<nc;j++){
+            printf("%d ",m[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int **malloc2dR(int nr, int nc){
+    int i, **m;
+    m = malloc(nr*sizeof(int*));
+    for (i = 0; i<nr; i++){
+        m[i] = malloc(nc*sizeof (int));
+    }
+    return m;
+}
+
+void malloc2dP(int ***mp, int nr, int nc){
+    int i,j,**m;
+    m = malloc(nr*sizeof (int*));
+    for (i=0; i<nr;i++){
+        m[i] = malloc(nc*sizeof(int));
+    }
+    *mp = m;
 }
--- L02/L02_export/E01/main.c
+++ L02/L02_export/E01/main.c
@@ -2,13 +2,34 @@
 
 #define INPUTFILE_PATH "./in.txt"
 
+int gcd(int a, int b);
+
 int main(int argc, char ** argv){
-    char buf[100];
-    FILE *fp = fopen(INPUTFILE_PATH, "r");
-    fgets(buf, 100,fp);
 
-    printf("Hello Template!\n");
-    printf("%s", buf);
+    printf("gcd: %d",gcd(600,54));
+}
 
-    return 0;
+int gcd(int a, int b){
+
+    if (a<b){
+        int temp = b;
+        b = a;
+        a = temp;
+    }
+
+    if (b==0) return a;
+    if (b==1) return 1;
+
+    if ((a%2 == 0) && (b%2==0)){ // a,b even
+        return 2*gcd(a/2, b/2);
+    }
+    if (a%2==0 && b%2) { // a even, b odd
+        return gcd(a/2, b);
+    }
+    if (a%2 && b%2==0) { // a odd, b even
+        return gcd(a, b/2);
+    }
+    if (a%2 && b%2) { // a,b odd
+        return gcd((a-b)/2, b);
+    }
 }
