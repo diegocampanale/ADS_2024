b22921aa4bae0ed9b8b436e97d3d8ab4f5eb1eab
diegocampanale <campanale.die@gmail.com>
2024-11-19 17:56:30 +0100

    setup L06

---
+++ L06/cmake-build-debug/E00/CMakeFiles/E00.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/E00.dir/main.c.o"
+  "CMakeFiles/E00.dir/main.c.o.d"
+  "E00"
+  "E00.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/E00.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
